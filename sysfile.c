d14bd45b70703609637af8f9a86fe6576f8a5b66
Brahma <upadhyayasangharsha@gmail.com>
2024-10-19 19:18:45 +0545

    config

--- sysfile.c
+++ sysfile.c
@@ -6,7 +6,6 @@
 #include<stdlib.h>
 
 
-
 // For Config data's
 
 typedef struct 
@@ -14,6 +13,14 @@ typedef struct
     char telegram_token[128];
     char chat_id[64];
 } Config;
+
+int config_data(const char *filename,Config *config){
+    FILE *file = fopen(filename, "r");
+    if(file!){
+        perror("Error opening Config File");
+        return -1;
+    }
+}
 
 // For Telegram Bot
 void tele_msg(const char *message){}
