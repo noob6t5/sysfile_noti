99beef65951e4aa9ef3b87c79a2a981b1d66b5bf
Brahma <upadhyayasangharsha@gmail.com>
2024-10-19 19:06:43 +0545

    willbeback

---
+++ sysfile.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+#include<sys/inotify.h>
+#include<fcntl.h>
+#include<curl/curl.h>
+#include<stdlib.h>
+
+
+
+
+// For Telegram Bot
+void tele_msg(const char *message){}
+
+
+
+
+// Main Fucntion
+int main(void){}
