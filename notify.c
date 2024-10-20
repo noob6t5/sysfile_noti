6ad4ffc84d0341ddbb94a113a74645e2a188f8e9
Brahma <upadhyayasangharsha@gmail.com>
2024-10-20 06:57:21 +0545

    addedfeat

--- notify.c
+++ notify.c
@@ -6,6 +6,8 @@
 #include <fcntl.h>
 #include <curl/curl.h>
 
+
+
 #define EVENT_SIZE (sizeof(struct inotify_event))
 #define EVENT_BUF_LEN (1024 * (EVENT_SIZE + 16))
 // For Config data's
@@ -52,7 +54,6 @@ int config_data(const char *filename, Config *config)
     }
 }
 // For Telegram Bot.......
-
 int tele_msg(const char *message,const Config *config) {
     CURL *curl;
     CURLcode res; //return code
@@ -64,7 +65,6 @@ int tele_msg(const char *message,const Config *config) {
         perror("CURL ain't working ");
         return -1;
     }
-
     // http req
     curl_easy_setopt(curl, CURLOPT_URL, url);
     curl_easy_setopt(curl, CURLOPT_HTTPGET, 1L);
@@ -79,10 +79,93 @@ int tele_msg(const char *message,const Config *config) {
     curl_easy_cleanup(curl);
     return 0;
 }
+/*
+Event's Handling delete,update,move,create:
+ https://man7.org/linux/man-pages/man7/inotify.7.html
+*/
+void event_handle(const struct inotify_event *event,const Config *config){
+    if (event->len=0)
+    return;
+    char message[256] = {0};
+    if(event->mask&IN_CREATE){
+        snprintf(message, sizeof(message), "New File created: %s", event->name);
+    }
+    else if (event->mask & IN_DELETE)
+    {
+        snprintf(message, sizeof(message), "File deleted: %s", event->name);
+    }
+    else if (event->mask & IN_MODIFY)
+    {
+        snprintf(message, sizeof(message), "File modified: %s", event->name);
+    }
+    else if (event->mask & IN_MOVED_TO)
+    {
+        snprintf(message, sizeof(message), "File moved to: %s", event->name);
+    }
+    else if (event->mask & IN_MOVED_FROM)
+    {
+        snprintf(message, sizeof(message), "File moved from: %s", event->name);
+    }
+
+if (strlen(message) > 0)
+{
+    if (send_message(message, config) != 0)
+    {
+        fprintf(stderr, "Error: Failed to send message for event on file: %s\n", event->name);
+    }
+}
+}
 
 // Main Fucntion
 int main(void)
 {
+    Config config;
+    if (read_config("config.ini", &config) != 0)
+    {
+        fprintf(stderr, "Error: Failed to load config.\n");
+        return EXIT_FAILURE;
+    }
 
-    printf("Everything fine :)\n");
+    const char *path = "/usr/bin/home" if (read_config("config.ini", &config) != 0)
+    {
+        fprintf(stderr, "Error: Failed to load configuration. Exiting.\n");
+        return EXIT_FAILURE;
+    }
+
+int fd=inotify_init();
+if (fd<0){
+    perror("Error in inotify_init ....");
+    return EXIT_FAILURE;
+}
+int wd = inotify_add_watch(fd, path, IN_MODIFY | IN_CREATE | IN_DELETE | IN_MOVE);
+if (wd == -1)
+{
+    perror("Error: inotify_add_watch() failed");
+    close(fd);
+    return EXIT_FAILURE;
+}
+char buffer[EVENT_BUF_LEN];
+
+while (1)
+{
+    int length = read(fd, buffer, EVENT_BUF_LEN);
+    if (length < 0)
+    {
+        perror("Error: read() failed");
+        break;
+    }
+
+    for (int i = 0; i < length;)
+    {
+        struct inotify_event *event = (struct inotify_event *)&buffer[i];
+        handle_event(event, &config);
+        i += EVENT_SIZE + event->len;
+    }
+}
+
+inotify_rm_watch(fd, wd);
+close(fd);
+
+return EXIT_SUCCESS;
+printf("Everything fine :)\n");
 }
